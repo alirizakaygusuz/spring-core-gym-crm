35af9b41ce70dfb60ab4fcf1936967e396fe1925
alirizakaygusuz <ali.rizakaygusuz@hotmail.com>
2026-01-06 16:44:16 -0500

    Initial project setup

---
+++ src/main/java/com/alirizakaygusuz/gymcrm/Application.java
@@ -0,0 +1,7 @@
+package com.alirizakaygusuz.gymcrm;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello Spring Core Gym Crm Task!");
+    }
+}
