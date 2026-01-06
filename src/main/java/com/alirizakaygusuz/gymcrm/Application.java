39b65b28d4f4c28fde8158a64c718d039528bf82
alirizakaygusuz <ali.rizakaygusuz@hotmail.com>
2026-01-06 17:30:44 -0500

    Set up annotation-based Spring application context

--- src/main/java/com/alirizakaygusuz/gymcrm/Application.java
+++ src/main/java/com/alirizakaygusuz/gymcrm/Application.java
@@ -1,7 +1,16 @@
 package com.alirizakaygusuz.gymcrm;
 
+import com.alirizakaygusuz.gymcrm.config.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello Spring Core Gym Crm Task!");
+
+        try (AnnotationConfigApplicationContext context =
+                     new AnnotationConfigApplicationContext(AppConfig.class)) {
+
+            System.out.println("Hello Spring Core Gym Crm Task!");
+        }
     }
+
 }
---
+++ src/main/java/com/alirizakaygusuz/gymcrm/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.alirizakaygusuz.gymcrm.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.alirizakaygusuz.gymcrm")
+public class AppConfig {
+}
