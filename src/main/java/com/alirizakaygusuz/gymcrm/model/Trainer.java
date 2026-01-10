3e060f407beff29ec82bca6ef74610349853f3ed
alirizakaygusuz <ali.rizakaygusuz@hotmail.com>
2026-01-09 21:44:31 -0500

    refactor(trainer): Refactor Trainer entity specialization modeling

--- src/main/java/com/alirizakaygusuz/gymcrm/model/Trainer.java
+++ src/main/java/com/alirizakaygusuz/gymcrm/model/Trainer.java
@@ -2,50 +2,39 @@ package com.alirizakaygusuz.gymcrm.model;
 
 public class Trainer extends  User {
 
-    private String specialization;
-    private TrainingType trainingType;
+    private TrainingType specialization;
 
 
     public Trainer() {
     }
 
-    public Trainer(String firstName, String lastName, String username, String password, boolean isActive, String specialization, TrainingType trainingType) {
+    public Trainer(String firstName, String lastName, String username, String password, boolean isActive, TrainingType specialization) {
         super(firstName, lastName, username, password, isActive);
         this.specialization = specialization;
-        this.trainingType = trainingType;
     }
 
-    public Trainer(Long id, String firstName, String lastName, String username, String password, boolean isActive, String specialization, TrainingType trainingType) {
+    public Trainer(Long id, String firstName, String lastName, String username, String password, boolean isActive, TrainingType specialization) {
         super(id, firstName, lastName, username, password, isActive);
         this.specialization = specialization;
-        this.trainingType = trainingType;
     }
 
 
-
-    public String getSpecialization() {
+    // Getters and Setters
+    public TrainingType getSpecialization() {
         return specialization;
     }
-
-    public void setSpecialization(String specialization) {
+    public void setSpecialization(TrainingType specialization) {
         this.specialization = specialization;
     }
 
-    public TrainingType getTrainingType() {
-        return trainingType;
-    }
 
-    public void setTrainingType(TrainingType trainingType) {
-        this.trainingType = trainingType;
-    }
 
 
     @Override
     public String toString() {
         return "Trainer{" +
                 super.toString() +", " +
-                "specialization='" + specialization + '\'' +
-                ", trainingType=" + trainingType +
+                " specialization='" + specialization +
                 '}';
     }
 }
