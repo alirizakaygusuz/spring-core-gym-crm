58e22bd72faca493e9617ea97f8f41b5bd9267d2
alirizakaygusuz <ali.rizakaygusuz@hotmail.com>
2026-01-19 16:08:27 -0500

    refactor: integrate Lombok for model classes and simplify boilerplate code

--- src/main/java/com/alirizakaygusuz/gymcrm/model/User.java
+++ src/main/java/com/alirizakaygusuz/gymcrm/model/User.java
@@ -1,91 +1,21 @@
 package com.alirizakaygusuz.gymcrm.model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public abstract class User {
 
     private Long id;
     private String firstName;
     private String lastName;
     private String username;
+
+    @ToString.Exclude
     private String password;
-    private boolean isActive;
+    private boolean active;
 
-
-    protected User() {
-    }
-
-    protected User(String firstName, String lastName, String username, String password, boolean isActive) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.username = username;
-        this.password = password;
-        this.isActive = isActive;
-    }
-
-    protected User(Long id, String firstName, String lastName, String username, String password, boolean isActive) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.username = username;
-        this.password = password;
-        this.isActive = isActive;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public boolean isActive() {
-        return isActive;
-    }
-
-    public void setActive(boolean active) {
-        isActive = active;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", username='" + username + '\'' +
-                ", isActive=" + isActive +
-                '}';
-    }
 }
