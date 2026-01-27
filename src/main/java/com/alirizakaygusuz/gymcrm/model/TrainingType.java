a63e34eb1b4c64b7f824a24ceea91ee35b17116a
alirizakaygusuz <ali.rizakaygusuz@hotmail.com>
2026-01-27 22:19:39 +0300

    fix(jpa): apply @Enumerated mapping for TrainingTypeCode and align it with seed data

--- src/main/java/com/alirizakaygusuz/gymcrm/model/TrainingType.java
+++ src/main/java/com/alirizakaygusuz/gymcrm/model/TrainingType.java
@@ -6,7 +6,7 @@ import lombok.*;
 @Entity
 @Table(name = "training_types")
 @Getter
-@Setter
+@Sette
 @NoArgsConstructor
 @AllArgsConstructor
 public class TrainingType {
@@ -15,6 +15,7 @@ public class TrainingType {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "training_type_name", nullable = false, unique = true, length = 100)
-    private String trainingTypeName;
+    private TrainingTypeCode trainingTypeName;
 }
